57de1e38edc8423289311ce3c7fc333ffb1097ea
Gina Yuan <gina.yuan@gmail.com>
2023-11-08 18:08:54 -0700

    update paths after repo refactor

--- make_sidecar.sh
+++ make_sidecar.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -e
-cd $HOME/sidecar/quiche/
+export QUICHE_HOME=$HOME/sidecar/http3_integration/quiche
+cd $QUICHE_HOME
 make sidecar
 cd $HOME/sidecar/curl/
 autoreconf -fi
-./configure LDFLAGS="-Wl,-rpath,$HOME/sidecar/quiche/target/release" \
-    --with-openssl=$HOME/sidecar/quiche/quiche/deps/boringssl/src \
-    --with-quiche=$HOME/sidecar/quiche/target/release
+./configure LDFLAGS="-Wl,-rpath,$QUICHE_HOME/target/release" \
+    --with-openssl=$QUICHE_HOME/quiche/deps/boringssl/src \
+    --with-quiche=$QUICHE_HOME/target/release
 make -j4
 
